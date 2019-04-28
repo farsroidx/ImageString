5205b605d2ebf9cd4d47b48bd4a5bd8db6051ade
Mohammad Ali Riazati <>
2019-04-28 09:59:52 +0430

    Initial commit
    Initial commit

---
+++ library/src/main/java/ir/farsroidx/ImageString.java
@@ -0,0 +1,20 @@
+package ir.farsroidx;
+
+import android.graphics.Bitmap;
+
+public final class ImageString {
+
+    public ImageString() {
+
+    }
+
+    public String convertToString(){
+
+        return "";
+    }
+
+    public Bitmap convertToBitmap(){
+
+        return null;
+    }
+}

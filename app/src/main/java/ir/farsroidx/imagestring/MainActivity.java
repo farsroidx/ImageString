ae308857d8e20fbe3c71e1fe072f8d601fc1dcc0
Mohammad Ali Riazati <>
2019-04-28 10:24:54 +0430

    Initial commit
    Initial commit

--- app/src/main/java/ir/farsroidx/imagestring/MainActivity.java
+++ app/src/main/java/ir/farsroidx/imagestring/MainActivity.java
@@ -1,7 +1,13 @@
 package ir.farsroidx.imagestring;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import ir.farsroidx.ImageString;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = findViewById(R.id.img);
+
+        ImageString imageString = new ImageString(this);
+        String encode = imageString.encodeToString(R.drawable.photo);
+        Log.d("encodeString" , encode);
+        Bitmap decode = imageString.decodeToBitmap(encode);
+        img.setImageBitmap(decode);
+
     }
 }
--- library/src/main/java/ir/farsroidx/ImageString.java
+++ library/src/main/java/ir/farsroidx/ImageString.java
@@ -1,20 +1,39 @@
 package ir.farsroidx;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
 
 public final class ImageString {
 
-    public ImageString() {
+    private Context context;
 
+    public ImageString(Context context) {
+        this.context = context;
     }
 
-    public String convertToString(){
-
-        return "";
+    public String encodeToString(@DrawableRes int drawableRes){
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources() , drawableRes);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG , 100 , byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes , Base64.DEFAULT);
     }
 
-    public Bitmap convertToBitmap(){
+    public String encodeToString(@NonNull Bitmap bitmap){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG , 100 , byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes , Base64.DEFAULT);
+    }
 
-        return null;
+    public Bitmap decodeToBitmap(String imageString){
+        byte[] decodeString = Base64.decode(imageString , Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodeString , 0 , decodeString.length);
     }
 }
